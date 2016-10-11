32f2741956df7ccc09647f0e9ba4c56b843084d7
t-mangoe <t-mangoe@ist.osaka-u.ac.jp>
2016-10-11 15:00:20 +0900

    rbファイルの修正と、レポートの追加

--- lib/hello_trema.rb
+++ lib/hello_trema.rb
@@ -1,10 +1,15 @@
 # Hello World!
 class HelloTrema < Trema::Controller
   def start(_args)
-    logger.info 'Trema started.'
+    #logger.info "Trema started (args = #{args.inspect})."
+    logger.info "#{self.class} started."
   end
 
   def switch_ready(datapath_id)
     logger.info "Hello #{datapath_id.to_hex}!"
+  end
+
+  def switch_disconnected(datapath_id)
+    logger.info "Bey #{datapath_id.to_hex}"
   end
 end
